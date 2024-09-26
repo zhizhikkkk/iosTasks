c1dca72fd8c8a240f9e2c3bde46a4aeebe48fc53
Zhanel <98708550+zhizhikkkk@users.noreply.github.com>
2024-09-27 00:27:13 +0500

    feat: added Rich app project
    Assignment 3.1 done

---
+++ Rich/Rich/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Rich
+//
+//  Created by Arman Sarvardin on 21.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+#Preview {
+    ViewController()
+}
